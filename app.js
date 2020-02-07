8a2e5f62c82a64fb499120c4d07f9d22fa4eafa9
Jerry L Hoover <jerry.hoover@ymail.com>
2020-02-06 20:22:13 -0600

    testing DB

--- app.js
+++ app.js
@@ -7,11 +7,23 @@ const sequelize = new Sequelize({
 
 // movie model
 class Movie extends Sequelize.Model {}
-Movie.init();
+Movie.init(
+  {
+    title: Sequelize.STRING
+  },
+  { sequelize }
+); // same as {sequelize: sequelize}
 
 // async IIFE
 (async () => {
+  // sync "Movies" table
+  await sequelize.sync({ force: true });
   try {
+    // instance of Movie class represents a database row
+    const movie = await Movie.create({
+      title: "Toy Story"
+    });
+    console.log(movie.toJSON());
     await sequelize.authenticate();
     console.log("Connected to database...");
   } catch (error) {
